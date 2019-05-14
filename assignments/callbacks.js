15bcd3fba430064285041a4582a6da74bae44d52
hcopasstarke <hcopasstarke@gmail.com>
2019-05-14 13:31:29 -0700

    i hate being creative in a time crunch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,9 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+function firstItem(arr, cb) {
+  cb(arr[0]);
+}
 
 /* 
 
@@ -9,6 +12,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
+
 
   // Potential Solution:
 
@@ -27,23 +31,33 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 }
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.includes(item)) {
+    return cb(true);
+  } else {
+    return cb(false);
+  }
 }
 
 /* STRETCH PROBLEM */
